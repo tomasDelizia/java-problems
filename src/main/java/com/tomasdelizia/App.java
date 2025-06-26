2217b5ad649918dd0f670e396b46d01c30d53f69
tdeliziae <tdeliziae@eservicios.indra.es>
2025-06-26 10:59:39 +0200

    Initial commit

---
+++ src/main/java/com/tomasdelizia/App.java
@@ -0,0 +1,13 @@
+package com.tomasdelizia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
