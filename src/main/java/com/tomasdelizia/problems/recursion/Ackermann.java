6005c4cb501790d5e0819162a897b5fe3a4b0ded
Tomás Delizia <tomidelizia@gmail.com>
2025-06-27 00:38:40 +0200

    Add Ackermann function implementation and corresponding tests

---
+++ src/main/java/com/tomasdelizia/problems/recursion/Ackermann.java
@@ -0,0 +1,9 @@
+package com.tomasdelizia.problems.recursion;
+
+public class Ackermann {
+    public static int ackermann(int m, int n) {
+        if (m == 0) return n + 1;
+        if (n == 0) return ackermann(m - 1, 1);
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
