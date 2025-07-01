fe5c8ea47b34721ec995e774c4175ba58c8b873b
Tomás Delizia Elorza <tomidelizia@gmail.com>
2025-07-01 16:57:09 +0200

    Add BinarySearchTree class with insert and contains methods; include unit tests

---
+++ src/main/java/com/tomasdelizia/tree/BinarySearchTree.java
@@ -0,0 +1,114 @@
+package com.tomasdelizia.tree;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class BinarySearchTree<T> {
+    private Node<T> root;
+    private Comparator<? super T> comparator;
+
+    public BinarySearchTree(T value, Comparator<? super T> comparator) {
+        root = new Node<>(value);
+        this.comparator = comparator;
+    }
+
+    public BinarySearchTree(T value) {
+        root = new Node<>(value);
+    }
+
+    public BinarySearchTree() {
+    }
+
+    public boolean insert(T value) {
+        Node<T> node = new Node<>(value);
+        if (root == null) {
+            root = node;
+            return true;
+        }
+        Node<T> current = root;
+        while (true) {
+            int compare = compare(value, current.value);
+            // Can't insert if it already exists
+            if (compare == 0) {
+                return false;
+            }
+            // The inserted value is less than the current node, insert to the left
+            else if (compare < 0) {
+                // Add the node to the left branch if it's empty
+                if (current.left == null) {
+                    current.left = node;
+                    return true;
+                }
+                // Else, we update the current node to that of the node on the left
+                current = current.left;
+            }
+            // The inserted value is greater than the current node, insert to the right
+            else {
+                if (current.right == null) {
+                    current.right = node;
+                    return true;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    public boolean contains(T value) {
+        if (root == null) {
+            return false;
+        }
+        Node<T> current = root;
+        while (current != null) {
+            int compare = compare(value, current.value);
+            if (compare == 0) {
+                return true;
+            } else if (compare < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    private int compare(Object k1, Object k2) {
+        return (comparator != null) ? comparator.compare((T) k1, (T) k2)
+                : ((Comparable<? super T>) k1).compareTo((T) k2);
+    }
+
+    public static class Node<T> {
+        private final T value;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value.toString();
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 61 * hash + this.value.hashCode();
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null) {
+                return false;
+            }
+            if (this.getClass() != o.getClass()) {
+                return false;
+            }
+            return Objects.equals(this.value, ((Node<?>) o).value);
+        }
+    }
+}
---
+++ src/test/java/com/tomasdelizia/tree/BinarySearchTreeTest.java
@@ -0,0 +1,45 @@
+package com.tomasdelizia.tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BinarySearchTreeTest {
+    @Test
+    void testInsertNode() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        assertTrue(bst.insert(10));
+        assertTrue(bst.insert(5));
+        assertTrue(bst.insert(15));
+        assertTrue(bst.insert(3));
+        assertTrue(bst.insert(7));
+        assertTrue(bst.insert(12));
+        assertTrue(bst.insert(18));
+        assertFalse(bst.insert(10)); // Duplicate insert should return false
+        assertFalse(bst.insert(18));  // Duplicate insert should return false
+    }
+
+    @Test
+    void testContains() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+        bst.insert(3);
+        bst.insert(7);
+        bst.insert(12);
+        bst.insert(18);
+
+        assertTrue(bst.contains(10));
+        assertTrue(bst.contains(5));
+        assertTrue(bst.contains(15));
+        assertTrue(bst.contains(3));
+        assertTrue(bst.contains(7));
+        assertTrue(bst.contains(12));
+        assertTrue(bst.contains(18));
+        assertFalse(bst.contains(20)); // Value not in the tree
+        assertFalse(bst.contains(1));  // Value not in the tree
+    }
+
+}
