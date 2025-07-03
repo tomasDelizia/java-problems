6c1a4bd530ea0ab03677d4e6613c885ea9dfcfb2
Tomás Delizia Elorza <tomidelizia@gmail.com>
2025-07-03 14:30:59 +0200

    Add Selection Sort strategy and corresponding unit tests

---
+++ src/main/java/com/tomasdelizia/sort/SelectionSortStrategy.java
@@ -0,0 +1,24 @@
+package com.tomasdelizia.sort;
+
+
+public class SelectionSortStrategy<T> extends BaseSortStrategy<T> {
+    public SelectionSortStrategy() {
+        super();
+    }
+
+    @Override
+    public void sort(T[] values) {
+        int n = values.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int compare = compare(values[i], values[j]);
+                if (compare < 0) {
+                    continue;
+                }
+                T larger = values[i];
+                values[i] = values[j];
+                values[j] = larger;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/tomasdelizia/sort/SelectionSortStrategyTest.java
@@ -0,0 +1,38 @@
+package com.tomasdelizia.sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SelectionSortStrategyTest {
+    private final ArraySorter<Integer> sorter = new SelectionSortStrategy<>();
+    
+    @Test
+    void insertionSortWithEmptyArray() {
+        Integer[] nums = {};
+        sorter.sort(nums);
+        assertArrayEquals(new Integer[]{}, nums);
+    }
+
+    @Test
+    void insertionSortWithSingleElementArray() {
+        Integer[] nums = {5};
+        sorter.sort(nums);
+        assertArrayEquals(new Integer[]{5}, nums);
+    }
+
+    @Test
+    void insertionSortWithMultipleElements() {
+        Integer[] nums = {5, 3, 8, 1, 2};
+        sorter.sort(nums);
+        assertArrayEquals(new Integer[]{1, 2, 3, 5, 8}, nums);
+
+        nums = new Integer[]{10, -1, 0, 5};
+        sorter.sort(nums);
+        assertArrayEquals(new Integer[]{-1, 0, 5, 10}, nums);
+
+        nums = new Integer[]{7, 7, 7};
+        sorter.sort(nums);
+        assertArrayEquals(new Integer[]{7, 7, 7}, nums);
+    }
+}
